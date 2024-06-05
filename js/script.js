d79bceb41a311a059d8f38058c368c8c76e67f40
Raka Pratama <mraka2006@gmail.com>
2024-06-05 23:26:47 +0700

    hampir final (kurang form)

--- js/script.js
+++ js/script.js
@@ -1,1 +1,31 @@
-// Ini JavaScript
+// Replace Name
+function replaceName() {
+    let name = prompt("Hello, what do you want to be called?", "");
+    document.getElementById("name").innerHTML = name;
+}
+
+
+replaceName();
+
+// Image Banner
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+    showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+    var i;
+    var x = document.getElementsByClassName("img-slideshow");
+    if (n > x.length) {slideIndex = 1}
+    else if (n < 1) {slideIndex = x.length} ;
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    x[slideIndex-1].style.display = "block";
+}
+
+setInterval(() =>{
+    plusDivs(1);
+}, 2000);   
